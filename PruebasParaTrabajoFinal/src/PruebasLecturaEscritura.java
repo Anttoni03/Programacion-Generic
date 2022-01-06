1aedb30febf1e0b450ca12124746aaef334278f4
Anttoni03 <tonifg2003@gmail.com>
2022-01-06 21:42:35 +0100

    Ni idea de que ha cambiado la vd

---
+++ PruebasParaTrabajoFinal/src/PruebasLecturaEscritura.java
@@ -0,0 +1,62 @@
+
+import java.io.FileReader;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tonif
+ */
+public class PruebasLecturaEscritura {
+    //método main
+    public static void main(String [] argumentos) throws Exception {
+        new PruebasLecturaEscritura().metodoPrincipal();
+    }
+    
+    //método metodoPrincipal
+    private void metodoPrincipal() throws Exception
+    {
+        int l;
+        int i;
+        FileReader lector = new FileReader("ficheroFichas.txt");
+        System.out.println((int)'\n');
+        l = lector.read();
+        
+        for (i = 0; l != -1;)
+        {
+            if (l == 13) i++;
+            
+            if (isLetOrNum((char)l))
+                System.out.println((char)l);
+            else
+            {
+                System.out.print("\t");
+                System.out.println(l);
+            }
+            
+            l = lector.read();
+        }
+        
+        System.out.println("Cantidad: " + i);
+        lector.close();
+    }
+    
+    private boolean isLetOrNum(char c)
+    {
+        char[] letters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        char[] numbers = "0123456789".toCharArray();
+        boolean notFound = true;
+        for (int i = 0; i < letters.length && notFound; i++) {
+            notFound = notFound && (c != letters[i]);
+        }
+        for (int i = 0; i < numbers.length && notFound; i++) {
+            notFound = notFound && (c != numbers[i]);
+        }
+        
+        return !notFound;
+    }
+}
