57cda0e43fc6b008b8f6b00799470b4c09639d1f
Anttoni03 <tonifg2003@gmail.com>
2022-01-12 20:51:13 +0100

    Ni idea

--- PruebasParaTrabajoFinal/src/PruebasRandom.java
+++ PruebasParaTrabajoFinal/src/PruebasRandom.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.util.Random;
 
@@ -21,6 +16,6 @@ public class PruebasRandom {
     {
         Random rand = new Random();
         
-        System.out.println(rand.nextInt(1));
+        System.out.println(rand.nextInt(10));
     }
 }
