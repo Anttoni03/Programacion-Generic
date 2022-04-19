989551b75e69e3abe7affc6601b01a596b502352
Anttoni03 <tonifg2003@gmail.com>
2022-04-19 17:35:53 +0200

    Acabar definitivamente (si es legal)

--- Prácticas/Taller1 Antoni Frau Gordiola/src/Taller1/Album.java
+++ Prácticas/Taller1 Antoni Frau Gordiola/src/Taller1/Album.java
@@ -2,24 +2,36 @@ package Taller1;
 
 public class Album {
     
+    //DECLARACIÓN DE ATRIBUTOS
+    //Atributo de objeto que define la posición del álbum
     private int posicion;
+    //Atributo de objeto que define el año del álbum (en catalán para no usar
+    //el carácter 'ñ'
     private int any;
+    //Atributo de objeto que define el título del álbum
     private String titulo;
+    //Atributo de objeto que define el artista del álbum
     private String artista;
+    //Atributo de objeto que define el tipo del álbum
     private String tipo;
+    //Atributo de objeto que define las estrellas del álbum
     private double estrellas;
+    //Atributo de objeto que define las ediciones hechas del álbum
     private int ediciones;
-    
+    //Atributo de clase que define la cantidad de líneas que se usan para
+    //representar un objeto álbum en el fichero donde se almacenan
     private static final int CANTIDAD_LINEAS = 7;
     
-    //MÉTODOS CONSTRUCTORES
+    
+    //MÉTODOS
+    //Método constructor para instanciar objetos Album
     public Album()
     {
         posicion = 0;
     }
     
-    
     //MÉTODOS FUNCIONALES
+    //Métodos setters de los atributos de objeto
     public void setPosicion(int posicion)
     {
         this.posicion = posicion;
@@ -49,6 +61,7 @@ public class Album {
         this.ediciones = ediciones;
     }
     
+    //Métodos getters de los atributos de objeto
     public int getPosicion()
     {
         return posicion;
@@ -79,11 +92,14 @@ public class Album {
     }
     
     
+    //Método getter de clase para el atributo CANTIDAD_LINEAS
     public static int getCantidadLineas()
     {
         return CANTIDAD_LINEAS;
     }
     
+    //Método toString para generar un String con los datos del álbum
+    @Override
     public String toString()
     {
         String resultado = "Album{num="+posicion+", "
