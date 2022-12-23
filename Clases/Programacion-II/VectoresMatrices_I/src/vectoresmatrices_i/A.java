07718a52af09b59a37cf06f4940aa7e3d16653b7
Anttoni03 <tonifg2003@gmail.com>
2022-12-23 23:41:48 +0100

    Algo

---
+++ Clases/Programacion-II/VectoresMatrices_I/src/vectoresmatrices_i/A.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vectoresmatrices_i;
+
+/**
+ *
+ * @author tonif
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println(((-43) % 6));
+    }
+}
